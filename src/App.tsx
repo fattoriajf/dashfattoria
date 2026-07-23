bac5d813ef455dfd500f9ed141fb3b89d1673294
fattoriajf <fattoriajf@gmail.com>
2026-07-22 23:50:40 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4219,7 +4219,7 @@ function EtiquetasTab() {
           type: 'pixel',
           format: 'image',
           flavor: 'base64',
-          data: `data:image/png;base64,${base64}`,
+          data: base64,
         }]);
       }
 
